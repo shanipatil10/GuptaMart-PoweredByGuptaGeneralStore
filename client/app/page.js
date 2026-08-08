c27e7726eaafb212b4e26af4103d9e371960ca7b
Shani Patil <shanipatil45@gmail.com>
2026-08-08 13:42:17 +0530

    Setup of FirebaseAuthentication

--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,13 @@
-
-import Navbar from "@/components/layout/Navbar";
+import { auth } from "@/lib/firebase";
 
 export default function Home() {
+  console.log(auth);
+
   return (
-    <main>
-      <Navbar />
-      <h1>GuptaMart</h1>
+    <main className="p-8">
+      <h1 className="text-2xl font-semibold">
+        GuptaMart
+      </h1>
     </main>
   );
 }
